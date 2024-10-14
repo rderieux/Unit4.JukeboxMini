d171cc3cf14e6515c9ad728d67b359dcb313432e
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-14 11:59:17 -0600

    Add api folder, import express router, call it, export router, import users.js into the app

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// morgan is logging middleware maintained by the Expressjs team
+// see: https://expressjs.com/en/resources/middleware/morgan.html
+app.use(require("morgan")("dev"));
+
+app.use(express.json());
+
+app.use("/users", require("./api/users"));
+
 app.listen(PORT, () => {
   console.log(`Listening on port#: ${PORT}...`);
 });
