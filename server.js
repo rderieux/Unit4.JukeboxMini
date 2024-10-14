9d8c6218ac7a0541c91a956a60b163158ea8d851
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-14 12:02:40 -0600

    Add 404 and error-handling middleware

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ app.use(express.json());
 
 app.use("/users", require("./api/users"));
 
+// 404 handling middleware
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: "The endpoint you are trying to reach does not exist.",
+  });
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something broke on the server side.");
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port#: ${PORT}...`);
 });
