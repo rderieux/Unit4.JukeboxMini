58bd79a9e9705a2d01542ae9714db4bc6f1972cb
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-14 11:46:39 -0600

    Allow us to seed the db with dummy data, generated through the faker library

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,14 +5,14 @@ const seed = async (numUsers = 3, numPlaylists = 5) => {
   // Loop through 3 users
   for (let i = 0; i < numUsers; i++) {
     //Use Array.from to create 5 playlists while looping through the 3 users
-    const playlists = Array.from({ length: numPlaylists }, () => ({
+    const playlists = Array.from({ length: numPlaylists }, (_, j) => ({
       name: faker.company.buzzAdjective() + " " + faker.company.buzzNoun(),
       description: faker.lorem.sentence(),
     }));
 
     await prisma.user.create({
       data: {
-        name: faker.person.fullName(),
+        username: faker.person.fullName(),
         playlists: {
           create: playlists,
         },
